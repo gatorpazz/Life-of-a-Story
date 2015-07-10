b6593486ffa6ce24513cd4887417b6280c85d0cc
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-10 09:55:16 -0400

    Altered some styles for the registration page, started Auth service

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -2,6 +2,26 @@
 (function() {
     'use strict';
     angular.module('life-of-a-story')
-      .factory('Auth', function($firebase, FIREBASE_URL) {
-      }
-})();
+      .factory('Auth', function($firebase, FIREBASE_URL, $rootScope) {
+          var myFirebaseRef = new Firebase("FIREBASE_URL");
+
+          var Auth = {
+            register: function(user) {
+              return ref.createUser(user.email, user.password);
+            },
+            login: function(user) {
+              return auth.$login('password', user);
+            },
+            logout: function() {
+              auth.$logout();
+            },
+            resolveUser: function() {
+              return auth.$getCurrentUser();
+            },
+            signedIn: function() {
+              return !!Auth.user.provider;
+            },
+            user: {}
+          };
+        }
+      })();
