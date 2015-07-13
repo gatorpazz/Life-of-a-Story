3c9ef73545268c23d16bec924a035f1a8a339fe5
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-13 09:55:12 -0400

    Added some dummy data added some javascript to display that data in a modal I created hurray

--- app/scripts/loas.js
+++ app/scripts/loas.js
@@ -1,5 +1,5 @@
 /* global angular */
-(function(){
+(function() {
   'use strict';
 
   angular.module('life-of-a-story', ['ui.router', 'firebase'])
@@ -9,25 +9,27 @@
 
       $stateProvider
 
-        // HOME VIEW WITH LOGIN AND REGISTRATION
+      // HOME VIEW WITH LOGIN AND REGISTRATION
         .state('home', {
-            url: '/home',
-            templateUrl: 'views/home.html',
-            controller: 'UserController',
-            controllerAs: 'user'
+          url: '/home',
+          templateUrl: 'views/home.html',
+          controller: 'UserController',
+          controllerAs: 'user'
         })
         .state('registration', {
-            url: '/registration',
-            templateUrl: 'views/registration.html'
+          url: '/registration',
+          templateUrl: 'views/registration.html'
         })
         .state('myBooks', {
-            url: '/my-books',
-            templateUrl: 'views/my-books.html'
+          url: '/my-books',
+          templateUrl: 'views/my-books.html'
         })
         .state('addBook', {
           url: '/add-a-book',
-          templateUrl: 'views/addBook.html'
+          templateUrl: 'views/addBook.html',
+          controller: 'AddBookController',
+          controllerAs: 'abc'
         });
     });
-angular.module('life-of-a-story').constant('FIREBASE_URL', 'https://life-of-a-story.firebaseio.com/');
+  angular.module('life-of-a-story').constant('FIREBASE_URL', 'https://life-of-a-story.firebaseio.com/');
 })();
