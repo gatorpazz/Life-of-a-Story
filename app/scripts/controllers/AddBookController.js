bbdb9691526868504b039b57b550688c1609f343
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-13 18:24:42 -0400

    Installed restangular and wired up to index.html

--- app/scripts/controllers/AddBookController.js
+++ app/scripts/controllers/AddBookController.js
@@ -3,7 +3,7 @@
   'use strict';
 
   angular.module('life-of-a-story')
-    .controller('AddBookController', function($scope, $http) {
+    .controller('AddBookController', function($scope, Auth, $firebase, $http) {
       var self = this;
       this.books = [];
         $http.get("/api/sampledata/hyperion.json")
