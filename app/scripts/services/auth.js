4b4338dfb474786546b355727033a28f1b5f7cd6
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-13 17:29:14 -0400

    Refactored Auth service

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -2,10 +2,23 @@
 (function() {
   'use strict';
   angular.module('life-of-a-story')
-    .factory("Auth", ["$firebaseAuth",
-      function($firebaseAuth) {
-        var ref = new Firebase("https://docs-sandbox.firebaseio.com");
-        return $firebaseAuth(ref);
-      }
-    ]);
+   // authenticate users
+   .factory('Auth', function($firebaseAuth){
+     var ref = new Firebase('https://life-of-a-story.firebaseio.com');
+     return {
+       magicAuth: $firebaseAuth(ref),
+       googleLogin: function(){
+         ref.authWithOAuthPopup('google', function(){
+           console.log('hello');
+         }, {remember: 'sessionOnly'});
+       },
+       authStatus: function(){
+         return ref.getAuth();
+       },
+       ghLogout: function(){
+         ref.unauth();
+         console.log('goodbye');
+       }
+     }; // END Auth return
+   }); // END Auth factory
 })();
