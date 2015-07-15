4ee1b3d09520501088246a877cb10571493c9cc4
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-15 11:20:30 -0400

    Error will have to be fixed in next release

--- app/scripts/controllers/AddBookController.js
+++ app/scripts/controllers/AddBookController.js
@@ -18,7 +18,7 @@
       $scope.confirm = {};
       $scope.books = [];
       $scope.searchBooks = function() {
-        $http.get('https://www.googleapis.com/books/v1/volumes?q=++intitle:' + $scope.query.title + '+inauthor:' + $scope.query.author + '&access_token=' + $scope.user.google.accessToken)
+        $http.get('https://www.googleapis.com/books/v1/volumes?q=' + $scope.query.title + '+inauthor:' + $scope.query.author + '&access_token=' + $scope.user.google.accessToken)
           .then(function(response) {
             $scope.books = response.data;
           });
