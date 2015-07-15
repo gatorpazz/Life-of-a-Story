005714d7179f3a14457761fb611b192ad7abfcdb
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-15 13:58:21 -0400

    Made a MyBooksController.js file

---
+++ app/scripts/controllers/MyBooksController.js
@@ -0,0 +1,9 @@
+/* global angular */
+(function() {
+  'use strict';
+
+  angular.module('life-of-a-story')
+    .controller('MySummaryController', function($scope, Auth) {
+      $scope.user = Auth.authStatus();
+    }); // END MySummaryController
+})();
