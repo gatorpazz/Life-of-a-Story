7a5ab2e7bebb907080729108e5e24b0eb4818380
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-15 14:29:50 -0400

    Did something

--- app/scripts/controllers/MyBooksController.js
+++ app/scripts/controllers/MyBooksController.js
@@ -3,7 +3,9 @@
   'use strict';
 
   angular.module('life-of-a-story')
-    .controller('MySummaryController', function($scope, Auth) {
+    .controller('MyBooksController', function($scope, Auth, $firebaseArray) {
       $scope.user = Auth.authStatus();
+      var ref = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books');
+      $scope.userBooks = $firebaseArray(ref);
     }); // END MySummaryController
 })();
