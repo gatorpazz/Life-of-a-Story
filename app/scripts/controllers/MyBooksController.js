67776b96caafd37028df9d05e788d611a9d51676
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-15 16:05:34 -0400

    Added a function that deletes books from firebase

--- app/scripts/controllers/MyBooksController.js
+++ app/scripts/controllers/MyBooksController.js
@@ -7,5 +7,8 @@
       $scope.user = Auth.authStatus();
       var ref = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books');
       $scope.userBooks = $firebaseArray(ref);
+      $scope.deleteBook = function(book) {
+        $scope.userBooks.$remove(book);
+      }
     }); // END MySummaryController
 })();
