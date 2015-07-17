8084ee9c0adffc95a1f6ab794062630dde4c6c54
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-17 11:27:10 -0400

    Added some styles to hide the forms till you click the form to add Notes

--- app/scripts/controllers/NotesController.js
+++ app/scripts/controllers/NotesController.js
@@ -8,6 +8,10 @@
       $scope.user = Auth.authStatus();
       var books = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books/');
       $scope.book = $firebaseObject(books.child($stateParams.book));
+      $scope.tab = null;
+      $scope.openNoteForm = function(value) {
+        $scope.tab = value;
+      }
 
     }); // END GameResultController
 })();
