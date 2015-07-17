dfe0efd89bfced97b8de0b1b9df613a4bc0cdb31
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-17 12:21:17 -0400

    Created note objects for each note type in the controller

--- app/scripts/controllers/NotesController.js
+++ app/scripts/controllers/NotesController.js
@@ -9,13 +9,26 @@
       var books = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books/');
       $scope.book = $firebaseObject(books.child($stateParams.book));
       $scope.button = null;
+      $scope.genNote = {
+        'title': null,
+        'body': null
+      };
+      $scope.pageNote = {
+        'title': null,
+        'page': null,
+        'body': null
+      };
+      $scope.charNote = {
+        'character': null,
+        'body': null
+      };
       $scope.openNoteForm = function(value) {
         $scope.button = value;
-      }
+      };
       $scope.tab = 1;
       $scope.openNotes = function(tab) {
         $scope.tab = tab;
-      }
+      };
 
     }); // END GameResultController
 })();
