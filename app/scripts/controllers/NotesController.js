4f574747f7af7394dd8662f16c0119a251d1ea88
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-18 06:01:59 -0400

    Corrected the addNote function, created a reference to a notes section for each book, tested and works

--- app/scripts/controllers/NotesController.js
+++ app/scripts/controllers/NotesController.js
@@ -3,11 +3,13 @@
   'use strict';
 
   angular.module('life-of-a-story')
-    .controller('NotesController', function($scope, $firebase, Auth, $firebaseObject, $stateParams) {
+    .controller('NotesController', function($scope, $firebase, Auth, $firebaseObject, $firebaseArray, $stateParams) {
 
       $scope.user = Auth.authStatus();
       var books = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books/');
       $scope.book = $firebaseObject(books.child($stateParams.book));
+      var notes = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books/' + $stateParams.book + '/notes/');
+      $scope.notes = $firebaseArray(notes);
       $scope.button = null;
       $scope.genNote = {
         'title': null,
@@ -30,7 +32,7 @@
         $scope.tab = tab;
       };
       $scope.addNote = function(note) {
-        $scope.book.$add(note);
+        $scope.notes.$add(note);
       };
 
     }); // END GameResultController
