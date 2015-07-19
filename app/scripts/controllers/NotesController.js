bf98145643a097f6643e839652a44d0d63228549
Michael Pascuzzi <gatorpazz@gmail.com>
2015-07-19 01:02:12 -0400

    Refactored so that all notes go in a single notes part in fireBase, added a delete function that works

--- app/scripts/controllers/NotesController.js
+++ app/scripts/controllers/NotesController.js
@@ -8,30 +8,28 @@
       $scope.user = Auth.authStatus();
       var books = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books/');
       $scope.book = $firebaseObject(books.child($stateParams.book));
-      var genNotes = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books/' + $stateParams.book + '/notes/genNotes/');
-      var pageNotes = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books/' + $stateParams.book + '/notes/pageNotes/');
-      var charNotes = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books/' + $stateParams.book + '/notes/charNotes/');
-      var allNotes = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books/' + $stateParams.book + '/notes/allNotes/');
-      $scope.genNotes = $firebaseArray(genNotes);
-      $scope.pageNotes = $firebaseArray(pageNotes);
-      $scope.charNotes = $firebaseArray(charNotes);
-      $scope.allNotes = $firebaseArray(allNotes)
+      var notes = new Firebase('https://life-of-a-story.firebaseio.com/users/' + $scope.user.uid + '/books/' + $stateParams.book + '/notes/');
+      $scope.notes = $firebaseArray(notes)
+      console.log($scope.notes);
       $scope.button = null;
       $scope.genNote = {
         'title': null,
         'body': null,
-        'created_on': null
+        'created_on': null,
+        'gen': 'gen'
       };
       $scope.pageNote = {
         'title': null,
         'page': null,
         'body': null,
-        'created_on': null
+        'created_on': null,
+        'pag': 'pag'
       };
       $scope.charNote = {
         'character': null,
         'body': null,
-        'created_on': null
+        'created_on': null,
+        'char': 'char'
       };
       $scope.openNoteForm = function(value) {
         $scope.button = value;
@@ -40,20 +38,20 @@
       $scope.openNotes = function(tab) {
         $scope.tab = tab;
       };
-      $scope.addGenNote = function(note) {
+      $scope.addGenNote = function() {
         $scope.genNote.created_on = Date.now();
-        $scope.genNotes.$add($scope.genNote);
-        $scope.allNotes.$add($scope.genNote);
+        $scope.notes.$add($scope.genNote);
       };
       $scope.addPageNote = function() {
         $scope.pageNote.created_on = Date.now();
-        $scope.pageNotes.$add($scope.pageNote);
-        $scope.allNotes.$add($scope.pageNote);
+        $scope.notes.$add($scope.pageNote);
       };
       $scope.addCharNote = function() {
         $scope.charNote.created_on = Date.now();
-        $scope.charNotes.$add($scope.charNote);
-        $scope.allNotes.$add($scope.charNote);
+        $scope.notes.$add($scope.charNote);
+      };
+      $scope.deleteNote = function(note) {
+        $scope.notes.$remove(note);
       };
 
     }); // END GameResultController
